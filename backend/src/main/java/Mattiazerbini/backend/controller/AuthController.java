ebf44c897712bed742f70d4aa6e4d2de84ead164
Mattia Zerbini <zermatty6@gmail.com>
2026-03-05 15:38:10 +0100

    change generete token add ruolo in token

--- backend/src/main/java/Mattiazerbini/backend/controller/AuthController.java
+++ backend/src/main/java/Mattiazerbini/backend/controller/AuthController.java
@@ -7,12 +7,15 @@ import Mattiazerbini.backend.payloads.UtentePayload;
 import Mattiazerbini.backend.services.AuthService;
 import Mattiazerbini.backend.services.UtenteService;
 import Mattiazerbini.backend.Excenptions.ValidationException;
+import com.cloudinary.Url;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -29,23 +32,29 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseDTO login(@RequestBody LoginDTO body) {
         String token = this.authService.checkCredentialsAndGenerateToken(body);
-        Utente utente = this.authService.getUserByEmail(body.email());
-        return new ResponseDTO(token, utente.getRuolo().name());
+        return new ResponseDTO(token);
     }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public Utente createUser(@RequestBody @Validated UtentePayload payload, BindingResult validationResult) {
+    public ResponseEntity <?> createUser(@RequestBody @Validated UtentePayload payload, BindingResult validationResult) {
 
         if (validationResult.hasErrors()) {
             List<String> errorsList = validationResult.getFieldErrors()
                     .stream()
                     .map(fieldError -> fieldError.getDefaultMessage())
                     .toList();
-
-            throw new ValidationException(errorsList);
-        } else {
-            return this.utenteService.salvaUtente(payload);
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("message: ", errorsList));
+        }
+        try {
+            Utente newUtente = this.utenteService.salvaUtente(payload);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUtente);
+        }catch(RuntimeException ex){
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(Map.of("message: ", ex.getMessage()));
         }
 
     }
